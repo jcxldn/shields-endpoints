cf6090a64ba7600d3c7ff31ea1367f426c85113e
Prouser123 <jcahill773@gmail.com>
2019-02-23 21:05:40 +0000

    add initial site

---
+++ static/script.js
@@ -0,0 +1,57 @@
+const d = new Date();
+    
+const days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+];
+
+window.onload = function() {
+  row1();
+  row2();
+  row3();
+};
+
+function row1() {
+    const row1 = document.getElementById("row1");
+    
+    // Set badge source
+    row1.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2Fshields-endpoints.herokuapp.com%2Fis-it%2F${days[d.getDay()]}`;
+
+    // Set display link
+    row1.childNodes[1].childNodes[0].href = `https://shields-endpoints.herokuapp.com/is-it/${days[d.getDay()]}`
+    
+    // Set display address
+    row1.childNodes[1].childNodes[0].childNodes[0].innerHTML = `/is-it/${days[d.getDay()]}`
+}
+
+function row2() {
+    const row2 = document.getElementById("row2");
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    
+    // Set badge source
+    row2.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2Fshields-endpoints.herokuapp.com%2Fis-it%2F${days[tomorrow.getDay()]}`;
+
+    // Set display link
+    row2.childNodes[1].childNodes[0].href = `https://shields-endpoints.herokuapp.com/is-it/${days[tomorrow.getDay()]}`
+
+    // Set display address
+    row2.childNodes[1].childNodes[0].childNodes[0].innerHTML = `/is-it/${days[tomorrow.getDay()]}`
+}
+
+function row3() {
+    const row3 = document.getElementById("row3");
+    
+    // Set badge source
+    row3.childNodes[3].childNodes[0].src = 'https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2Fshields-endpoints.herokuapp.com%2Fis-it%2Finvalid';
+
+    
+    // Set display link
+    row3.childNodes[1].childNodes[0].href = 'https://shields-endpoints.herokuapp.com/is-it/invalid'
+}
