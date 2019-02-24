2bc092b4e28eeb454aea5fe2fabc007ddd98aa3f
Prouser123 <jcahill773@gmail.com>
2019-02-24 10:40:02 +0000

    add helper class

--- static/script.js
+++ static/script.js
@@ -12,61 +12,92 @@ const days = [
 
 const hostname = window.location.hostname;
 
+console.log('onload.DateAndTime: ' + d)
+
 window.onload = function() {
-    row1();
-    row2();
-    row3();
-    debug_row1();
-    debug_row2();
+    new table1;
+    new debugTable;
 };
 
-function row1() {
-    const row1 = document.getElementById("row1");
-    
-    // Set badge source
-    row1.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2F${days[d.getDay()]}`;
+class BaseRow {
+    constructor(row) {
+        this.row = row;
+    }
 
-    // Set display link (dynamic)
-    row1.childNodes[1].childNodes[0].href = `./is-it/${days[d.getDay()]}`
-    
-    // Set display address (dynamic)
-    row1.childNodes[1].childNodes[0].childNodes[0].innerHTML = `/is-it/${days[d.getDay()]}`
+    setBadgeSource(source) {
+        this.row.childNodes[3].childNodes[0].src = source;
+    }
+
+    setDisplayLink(source) {
+        this.row.childNodes[1].childNodes[0].href = source;
+    }
+
+    setDisplayAddress(source) {
+        this.row.childNodes[1].childNodes[0].childNodes[0].innerHTML = source;
+    }
+
+    setDisplayDual(source) {
+        this.setDisplayLink(source);
+        this.setDisplayAddress(source);
+    }
 }
 
-function row2() {
-    const row2 = document.getElementById("row2");
+class table1 {
+    constructor() {
+        this.constructor.row1();
+        this.constructor.row2();
+        this.constructor.row3();
+    }
 
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
+    static row1() {
+        const base = new BaseRow(document.getElementById("row1"));
+        
+        // Set badge source
+        base.setBadgeSource(`https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2F${days[d.getDay()]}`);
+
+        // Set display link & address (dynamic)
+        base.setDisplayDual(`/is-it/${days[d.getDay()]}`)
+    }
+
+    static row2() {
+        const base = new BaseRow(document.getElementById("row2"));
+
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        
+        // Set badge source
+        base.setBadgeSource(`https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2F${days[tomorrow.getDay()]}`);
     
-    // Set badge source
-    row2.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2F${days[tomorrow.getDay()]}`;
+        // Set display link & address (dynamic)
+        base.setDisplayDual(`/is-it/${days[tomorrow.getDay()]}`);
+    }
 
-    // Set display link (dynamic)
-    row2.childNodes[1].childNodes[0].href = `./is-it/${days[tomorrow.getDay()]}`
-
-    // Set display address (dynamic)
-    row2.childNodes[1].childNodes[0].childNodes[0].innerHTML = `/is-it/${days[tomorrow.getDay()]}`
+    static row3() {
+        const base = new BaseRow(document.getElementById("row3"));
+        
+        // Set badge source
+        base.setBadgeSource(`https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2Finvalid`);
+    }
 }
 
-function row3() {
-    const row3 = document.getElementById("row3");
+class debugTable {
+    constructor() {
+        this.constructor.row1();
+        this.constructor.row2();
+        this.constructor.row1();
+    }
+
+    static row1() {
+        const base = new BaseRow(document.getElementById("debug_row1"));
     
-    // Set badge source
-    row3.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fis-it%2Finvalid`;
-}
+        // Set badge source relative to hostname
+        base.setBadgeSource(`https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fdebug%2Froutes%2Fformatted`);
+    }
 
-function debug_row1() {
-    const row = document.getElementById("debug_row1");
-
-    // Set badge source relative to hostname
-    row.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fdebug%2Froutes%2Fformatted`;
-}
-
-
-function debug_row2() {
-    const row = document.getElementById("debug_row2");
-
-    // Set badge source relative to hostname
-    row.childNodes[3].childNodes[0].src = `https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fdebug%2Fdatetime%2Fformatted`;
+    static row2() {
+        const base = new BaseRow(document.getElementById("debug_row2"));
+    
+        // Set badge source relative to hostname
+        base.setBadgeSource(`https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2F${hostname}%2Fdebug%2Fdatetime%2Fformatted`);
+    }
 }
