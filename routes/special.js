b9287e6102c3cb8cb9d98a01767d46e0927fa027
Prouser123 <jcahill773@gmail.com>
2019-04-06 15:48:06 +0100

    [special] dogbomb1's last stand

--- routes/special.js
+++ routes/special.js
@@ -6,19 +6,34 @@ module.exports = function(app) {
     const baseURL = `https://api.prouser123.me/youtube/channel/username/`;
 
     request(baseURL + "pewdiepie", function(error, response, body) {
-        const pewds = JSON.parse(body).subscribers;
-        
-        request(baseURL + "tseries", function(error, response, body) {
-            const tseries = JSON.parse(body).subscribers;
+      const pewds = JSON.parse(body).subscribers;
 
-            res.send({
-                schemaVersion: 1,
-                label: 'sub gap',
-                message: metric(pewds - tseries),
-                color: "red",
-                namedLogo: "youtube"
-            });
+      request(baseURL + "tseries", function(error, response, body) {
+        const tseries = JSON.parse(body).subscribers;
+
+        res.send({
+          schemaVersion: 1,
+          label: "sub gap",
+          message: metric(pewds - tseries),
+          color: "red",
+          namedLogo: "youtube"
         });
+      });
+    });
+  });
+
+  app.get("/special/dogbomb1/lastratio", (req, res) => {
+    const url = "https://api.prouser123.me/twitter/status/1114286592737210368";
+    request(url, function(error, response, body) {
+      const json = JSON.parse(body);
+      res.send({
+        schemaVersion: 1,
+        label: `dogbomb1's last stand`,
+        message:
+          ((json.likes / json.user.followers) * 100).toString().split(".")[0] +
+          "% like/follower ratio",
+        color: "blue"
+      });
     });
   });
 };
--- tests/special.tester.js
+++ tests/special.tester.js
@@ -7,23 +7,42 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 chai.should();
 describe("Special", () => {
-    describe(`Pewdiepie vs T-Series Subscriber Gap`, () => {
-      let path = `/special/subgap`;
-      it(`GET ${path}`, done => {
-        chai
-          .request(app)
-          .get(path)
-          .end((err, res) => {
-            res.should.have.status(200);
-            chai.expect(res.body).to.include({
-              schemaVersion: 1,
-              label: "sub gap",
-              color: "red",
-              namedLogo: "youtube"
-            });
-            chai.expect(res.body.message).to.be.a("string");
-            done();
+  describe(`Pewdiepie vs T-Series Subscriber Gap`, () => {
+    let path = `/special/subgap`;
+    it(`GET ${path}`, done => {
+      chai
+        .request(app)
+        .get(path)
+        .end((err, res) => {
+          res.should.have.status(200);
+          chai.expect(res.body).to.include({
+            schemaVersion: 1,
+            label: "sub gap",
+            color: "red",
+            namedLogo: "youtube"
           });
-      });
+          chai.expect(res.body.message).to.be.a("string");
+          done();
+        });
     });
+  });
+
+  describe(`Dogbomb1's Last Stand Likes/Followers Ratio`, () => {
+    let path = `/special/dogbomb1/lastratio`;
+    it(`GET ${path}`, done => {
+      chai
+        .request(app)
+        .get(path)
+        .end((err, res) => {
+          res.should.have.status(200);
+          chai.expect(res.body).to.include({
+            schemaVersion: 1,
+            label: "dogbomb1's last stand",
+            color: "blue"
+          });
+          chai.expect(res.body.message).to.be.a("string");
+          done();
+        });
+    });
+  });
 });
