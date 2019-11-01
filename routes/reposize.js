fecb1d8d052eeac218ee09c80b102bbfc551cc86
James Cahill <jcahill773@gmail.com>
2019-11-01 12:15:15 +0000

    offload octokit reposize auth to api service

--- routes/reposize.js
+++ routes/reposize.js
@@ -1,12 +1,14 @@
-const reposize = require("reposize");
+const request = require("request");
+const { createRequestOptions } = require("../helpers");
 
 module.exports = function(app) {
   app.get("/reposize/:owner/:repo", (req, res) => {
-    reposize.GetPrettyBytes(req.params.owner, req.params.repo).then(data => {
+    const url = `https://api.prouser123.me/reposize/${req.params.owner}/${req.params.repo}`;
+    request(createRequestOptions(url), function(error, response, body) {
       res.send({
         schemaVersion: 1,
         label: "repo size",
-        message: data,
+        message: JSON.parse(body).size,
         color: "blue"
       });
     });
